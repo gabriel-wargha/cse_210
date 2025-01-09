6eeb133329fd5a0ab6525786870b0ad76a459457
gabriel-wargha <gabriel@wargha.com>
2025-01-09 16:46:50 -0700

    Prepare 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score: ");
+        float score = float.Parse(Console.ReadLine());
+        string letter;
+
+        if (score > 90 || score == 90)
+        {
+            letter = "A";
+        }
+        else if (score > 80 || score == 80)
+        {
+         letter = "B";
+        }
+         else if (score > 70 || score == 70)
+        {
+         letter = "C";
+        }
+         else if (score > 60 || score == 60)
+        {
+         letter = "D";
+        }
+        else 
+        {
+         letter = "F";
+        }
+
+        if (score > 70 || score == 70){
+            Console.WriteLine("Congratulations, you passed!!!");
+        }
+        else {
+            Console.WriteLine("You reproved, try next time");
+        }
+        Console.WriteLine($"Your score it was {score} and your grade {letter}");
     }
 }
